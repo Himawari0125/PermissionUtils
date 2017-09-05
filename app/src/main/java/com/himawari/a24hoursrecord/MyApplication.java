72589d067d9b7bcd9c37a125d31e0748eb0be38a
Himawari <faithinngu@163.com>
2017-09-05 18:40:59 +0800

    提交

---
+++ app/src/main/java/com/himawari/a24hoursrecord/MyApplication.java
@@ -0,0 +1,8 @@
+package com.himawari.a24hoursrecord;
+
+/**
+ * Created by S.Lee on 2017/9/5 0005.
+ */
+
+public class MyApplication {
+}
---
+++ app/src/main/java/com/himawari/a24hoursrecord/views/CircleView.java
@@ -0,0 +1,8 @@
+package com.himawari.a24hoursrecord.views;
+
+/**
+ * Created by S.Lee on 2017/9/5 0005.
+ */
+
+public class CircleView {
+}
